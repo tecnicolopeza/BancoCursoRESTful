f12ec894e9c450fd84a99386a64819dd9b809e06
tecnicoalopeza <tecnicoalopeza@gmail.com>
2024-02-20 13:05:00 +0100

    Video 05 terminado - Configuracion de Capa Application

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services) 
+        {
+            //Permite registrar automaticamento los mappeos que haga en esta biblioteca de clases
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //Las sentencias de validacion vendran dentro de esta biblioteca de clases
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            //Esto nos permitirá hacer la referencia de proyecto
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
